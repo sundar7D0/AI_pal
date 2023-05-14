de11c28ce4f28907f876f43965de1bd3ef93e9a8
Sundar Raman P <43384356+sundar7D0@users.noreply.github.com>
2023-05-14 21:48:11 +0530

    Update 1_🔗_Connectors.py

--- pages/1_🔗_Connectors.py
+++ pages/1_🔗_Connectors.py
@@ -46,8 +46,8 @@ def get_credentials():
     # created automatically when the authorization flow completes for the first
     # time.
 
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    #if os.path.exists("token.json"):
+    #    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
