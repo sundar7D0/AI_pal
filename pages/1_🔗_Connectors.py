33cab66e0a3146df3ef46572c4a032b2f7918217
Sundar Raman P <43384356+sundar7D0@users.noreply.github.com>
2023-05-14 21:49:52 +0530

    Update 1_🔗_Connectors.py

--- pages/1_🔗_Connectors.py
+++ pages/1_🔗_Connectors.py
@@ -54,7 +54,7 @@ def get_credentials():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES, redirect_uri="https://sundar7d0-gptpal--gptpal-kq1rdk.streamlit.app")
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=80)
         # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
